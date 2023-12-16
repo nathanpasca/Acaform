d9d8c9885d3a5d3646669797539d9d7118d2a519
William Nathanael <william.nathanael3520@gmail.com>
2023-12-17 00:18:44 +0700

    update

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -7,129 +7,119 @@ import { getDoc, doc } from "firebase/firestore";
 import { firestore } from "../firebase";
 
 const NavigationBar = () => {
-	const [user, setUser] = useState(null);
-	const [userData, setUserData] = useState(null);
+  const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null);
 
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, async (user) => {
-			setUser(user);
-			if (user) {
-				const userDoc = await getDoc(doc(firestore, "users", user.uid));
-				if (userDoc.exists()) {
-					const fetchedUserData = userDoc.data();
-					setUserData(fetchedUserData);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      setUser(user);
+      if (user) {
+        const userDoc = await getDoc(doc(firestore, "users", user.uid));
+        if (userDoc.exists()) {
+          const fetchedUserData = userDoc.data();
+          setUserData(fetchedUserData);
 
-					const userRole = fetchedUserData.role;
-				} else {
-					console.error("Dokumen pengguna tidak ditemukan di Firestore");
-				}
-			}
-		});
+          const userRole = fetchedUserData.role;
+        } else {
+          console.error("Dokumen pengguna tidak ditemukan di Firestore");
+        }
+      }
+    });
 
-		return () => unsubscribe();
-	}, []);
+    return () => unsubscribe();
+  }, []);
 
-	return (
-		<div className="navbar bg-base-100">
-			<div className="navbar-start">
-				<div className="dropdown">
-					<label tabIndex={0} className="btn btn-ghost lg:hidden">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor">
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M4 6h16M4 12h8m-8 6h16"
-							/>
-						</svg>
-					</label>
-					<ul
-						tabIndex={0}
-						className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-						<li>
-							<Link to="/">
-								<a>Home</a>
-							</Link>
-						</li>
-						<li>
-							{userData && userData.role === "lecturer" ? (
-								<Link to="/lecturer-dashboard">
-									<a>Dashboard</a>
-								</Link>
-							) : (
-								<Link to="/enrollment-form">
-									<a>Enrollment</a>
-								</Link>
-							)}
-						</li>
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <Link to="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              {userData && userData.role === "lecturer" ? (
+                <Link to="/lecturer-dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              ) : (
+                <Link to="/enrollment-form">
+                  <a>Enrollment</a>
+                </Link>
+              )}
+            </li>
 
-						<li>
-							<a>Profile</a>
-						</li>
+            <li>
+              <a>Profile</a>
+            </li>
 
-						<li>
-							<Link to="/materials">
-								<a>Materials</a>
-							</Link>
-						</li>
-					</ul>
-				</div>
-				<Link to="/">
-					<a className="btn btn-ghost text-xl">Acaform</a>
-				</Link>
-			</div>
-			<div className="navbar-center hidden lg:flex">
-				{user ? (
-					<ul className="menu menu-horizontal px-1">
-						<li>
-							<Link to="/">
-								<a>Home</a>
-							</Link>
-						</li>
-						<li>
-							{userData && userData.role === "lecturer" ? (
-								<Link to="/lecturer-dashboard">
-									<a>Dashboard</a>
-								</Link>
-							) : (
-								<Link to="/enrollment-form">
-									<a>Enrollment</a>
-								</Link>
-							)}
-						</li>
+            <li>
+              <Link to="/materials">
+                <a>Materials</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Link to="/">
+          <a className="btn btn-ghost text-xl">Acaform</a>
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        {user ? (
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              {userData && userData.role === "lecturer" ? (
+                <Link to="/lecturer-dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              ) : (
+                <Link to="/enrollment-form">
+                  <a>Enrollment</a>
+                </Link>
+              )}
+            </li>
 
-						<li>
-							<a>Profile</a>
-						</li>
+            <li>
+              <Link to="/profile">
+                <a>Profile</a>
+              </Link>
+            </li>
 
-						<li>
-							<Link to="/materials">
-								<a>Materials</a>
-							</Link>
-						</li>
-					</ul>
-				) : (
-					<ul className="menu menu-horizontal px-1"></ul>
-				)}
-			</div>
-			<div className="navbar-end">
-				{user ? (
-					// User is logged in, display LogoutButton
-					<Logout />
-				) : (
-					// User is not logged in, display Login button
-					<Link to="/login">
-						<a className="btn btn-ghost">Login</a>
-					</Link>
-				)}
-			</div>
-		</div>
-	);
+            <li>
+              <Link to="/materials">
+                <a>Materials</a>
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          <ul className="menu menu-horizontal px-1"></ul>
+        )}
+      </div>
+      <div className="navbar-end">
+        {user ? (
+          // User is logged in, display LogoutButton
+          <Logout />
+        ) : (
+          // User is not logged in, display Login button
+          <Link to="/login">
+            <a className="btn btn-ghost">Login</a>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default NavigationBar;
